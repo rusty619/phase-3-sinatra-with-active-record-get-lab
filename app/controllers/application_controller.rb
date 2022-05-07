be8b5d114286f9ff2c672b8423adef798c70efcb
Ruslan Khait <rusty6916@gmail.com>
2022-05-06 21:44:51 -0400

    commit tree 7c79cf57b3db6709974ca1a183c917bb79073021

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do 
+    # get all the barkeries from the database
+    barkies = Bakery.all
+    # send them back as a JSON array
+    barkies.to_json
+  end
+
+  get '/bakeries/:id' do 
+    barkies = Bakery.find(params[:id])
+    barkies.to_json(only: [:id,:name], 
+      include: {baked_goods: {only: [:id, :name, :price]}})
+  end
+
+  get '/baked_goods/by_price' do 
+    baked_goods = BakedGood.order("price desc")
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do 
+    baked_goods = BakedGood.order("price desc").first 
+    baked_goods.to_json
+  end
 
 end
